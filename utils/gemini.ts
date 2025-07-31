01a5cc67b2845f4d15c95359301f51e867916c45
nishit-27 <nishitiitr27@gmail.com>
2025-07-31 20:41:57 +0530

    changed to gemini 2.0 flash

--- utils/gemini.ts
+++ utils/gemini.ts
@@ -6,7 +6,7 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 export async function geminiTemplateResponse(userPrompt:string, systemPrompt:string) {
   const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
+    model: "gemini-2.0-flash",
     contents: userPrompt,
     config: {
       systemInstruction: systemPrompt,
@@ -18,7 +18,7 @@ export async function geminiTemplateResponse(userPrompt:string, systemPrompt:str
 
 export async function geminiChatResponse(userPrompt:string, basePrompt: string) {
   const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
+    model: "gemini-2.0-flash",
     contents: [{role:"user", text: BASE_PROMPT},{role:"user",text:"i have attached the sample code of a template that we are using. this is the starting point. make sure you make changes accordingly. This is the code:"+basePrompt},{role:"user",text:userPrompt}
     ],
     config: {
@@ -33,7 +33,7 @@ export async function geminiChatResponse(userPrompt:string, basePrompt: string) 
 
 export async function geminiFollowupChatResponse(llmMessage: MessageType[]) {
   const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
+    model: "gemini-2.0-flash",
     contents: llmMessage,
     config: {
       systemInstruction: getSystemPrompt(),
